93565cd4d4e401dcb55c0c0f1d582623984ca222
Rihel <569843207@qq.com>
2017-02-05 11:17:23 +0800

    finish

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+// 载入外挂
+var gulp = require('gulp'),
+	//sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	//jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename'),
+	clean = require('gulp-clean'),
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	cache = require('gulp-cache'),
+	livereload = require('gulp-livereload'),
+	htmlmin=require('gulp-htmlmin'),
+	scss=require('gulp-sass');
+//html
+gulp.task('common',function(){
+	return gulp.src('src/common/*.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true,
+			minifyJS: true,//压缩页面JS
+			minifyCSS: true//压缩页面CSS
+		}))
+		.pipe(gulp.dest('dist/common'))
+		.pipe(notify({ message: 'common task complete' }));
+})
+// 样式
+gulp.task('styles', function() {
+	return gulp.src('src/newcss/*.css')
+			   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+			   .pipe(gulp.dest('dist/newcss'))
+			   .pipe(minifycss())
+			   .pipe(gulp.dest('dist/newcss'))
+			   .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// 脚本
+gulp.task('scripts', function() {
+	return gulp.src('src/newjs/*.js')
+			   // .pipe(jshint('.jshintrc'))
+			   // .pipe(jshint.reporter('default'))
+			   // .pipe(concat('main.js'))
+			   // .pipe(gulp.dest('dist/scripts'))
+			   // .pipe(rename({ suffix: '.min' }))
+			   .pipe(uglify())
+			   .pipe(gulp.dest('dist/newjs'))
+			   .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// 图片
+gulp.task('images', function() {
+	return gulp.src('src/img/**/*')
+			   .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+			   .pipe(gulp.dest('dist/img'))
+			   .pipe(notify({ message: 'Images task complete' }));
+});
+
+// 清理
+gulp.task('clean', function() {
+	return gulp.src(['dist/newcss', 'dist/newjs'], {read: false})
+			   .pipe(clean());
+});
+
+// 预设任务
+gulp.task('default', ['clean','wapcss'],function() {
+	gulp.start('styles', 'scripts','common');
+});
+gulp.task('wapcss',()=>{
+	return gulp.src('src/wap/template/scss/*.scss')
+		.pipe(scss())
+		.pipe(gulp.dest('src/wap/template/css'))
+		.pipe(autoprefixer({
+			browsers:['last 2 version','ios 7','>1%']
+		}))
+		.pipe(gulp.dest('src/wap/template/css'))
+		.pipe(notify({message:'wap scss finish'}))
+})
+// 看手
+gulp.task('watch', function() {
+	gulp.watch('src/wap/template/scss/*.scss', ['wapcss']);
+	// 看守所有.scss档
+	//gulp.watch('src/newcss/*.css', ['styles']);
+
+	// 看守所有.js档
+	gulp.watch('src/newjs/*.js', ['scripts']);
+
+	// 看守所有图片档
+	gulp.watch('src/img/**/**.*', ['images']);
+
+	// 建立即时重整伺服器
+	var server = livereload();
+
+	// 看守所有位在 dist/  目录下的档案，一旦有更动，便进行重整
+
+});
